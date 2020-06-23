91ece3759587c1d4ae37e0402f54047d32b3a753
suhacanuluer <sculuer@gmail.com>
2020-06-23 21:23:28 +0300

    Database updated

--- Database/Database.js
+++ Database/Database.js
@@ -1,5 +1,10 @@
 const Sequelize = require("sequelize");
 const UserModel = require("../Models/User");
+const CostumerModel = require("../Models/Customer");
+const PaymentModel = require("../Models/Payment");
+const NoteModel = require("../Models/Note");
+const TotalModel = require("../Models/Total");
+
 const sequelize = new Sequelize("accountancy", "root", "Ass122...", {
     host: "127.0.0.1",
     dialect: "mysql",
@@ -23,6 +28,12 @@ var reconnectOptions = {
 };
 
 const User = UserModel(sequelize, Sequelize);
+const Customer = CostumerModel(sequelize, Sequelize);
+const Payment = PaymentModel(sequelize, Sequelize);
+const Note = NoteModel(sequelize, Sequelize);
+const Total = TotalModel(sequelize, Sequelize);
+
+
 
 module.exports = {
     sequelize,
