6854a7d6128f275e51bada3aa922966b63ffa857
suhacanuluer <sculuer@gmail.com>
2020-06-25 21:10:57 +0300

    notesRouter added in app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const Database = require("./Database/Database");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const customersRouter = require("./routes/customers");
-
+const notesRouter = require("./routes/notes"); 
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/customers", customersRouter);
-
+app.use("/notes", notesRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
