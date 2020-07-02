08217fa74bdd340618641301cf6b5cb22dd99f90
suhacanuluer <sculuer@gmail.com>
2020-07-02 13:41:55 +0300

    added CustomerBalance Model

---
+++ Models/CustomerBalance.js
@@ -0,0 +1,27 @@
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    return ( CustomerBalance = sequelize.define(
+        "customerbalance",
+        {
+            customerID: {
+                type: Sequelize.INTEGER,
+            },
+            inMoneyVAT: {
+                type: Sequelize.INTEGER
+            },
+            amountVAT: {
+                type: Sequelize.INTEGER,
+            },
+            inMoney: {
+                type: Sequelize.INTEGER
+            },
+            outMoney: {
+                type: Sequelize.INTEGER,
+            },
+            totalMoney: {
+                type: Sequelize.INTEGER
+            }
+        }
+    ))
+}
