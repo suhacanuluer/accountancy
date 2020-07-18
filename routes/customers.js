1f72fe2b720b393680f3b7b7df49429f7ed0a722
suhacanuluer <sculuer@gmail.com>
2020-07-18 17:11:46 +0300

    route updated

--- routes/customers.js
+++ routes/customers.js
@@ -1,40 +1,54 @@
 const express = require("express");
 const router = express.Router();
-const { Customer, User, UserBalance, sequelize } = require("../Database/Database");
+const { Customer, User, UserBalance, Payment, Note, CustomerBalance } = require("../Database/Database");
 
 router.get('/:id', (req, res) => {
   User.findOne({
     where: { id: req.params.id},
     attributes: [ "id", "companyInfo", "companyName", "fullName"],
     include: [
-      { model: Customer , attributes: [ "id", "userID", "customerName", "customerInfo", "phoneNumber"]},
-      { model: UserBalance }],
-  }).then(customer => {
-    res.json({
-      status: "success",
-      data: customer
-    });    
+      { model: Customer , attributes: [ "id", "userID", "customerName", "customerInfo", "phoneNumber" ]},
+      { model: UserBalance }
+    ]
+  }).then(customers => {
+    if(customers != null){
+      res.json({
+        status: "success",
+        data: customers
+      });
+    } else {
+      res.json({
+        status: "error",
+        message: "user not found"
+      });
+    }    
   });
 });
 
-// MÜŞTERİ DETAYLARI EKSİK (TABLO İLİŞKİLERİ YAPILACAK)
-// router.get("/:userID/details/:customerID", (req, res) => {
-//   Customer.findOne( {
-//     where: {
-//       userID: req.params.userID,
-//       id : req.params.customerID
-//     }
-//   }).then(customer => {
-//     if(customer) {
-//       res.send({
-//         status: "müşteri bulundu. Detaylar gösteriliyor.",
-//         customer: customer
-//       })
-//     } else {
-//       res.status(404).send({ status: "error", description: "Müşteri bulunamadı!!"})
-//     }
-//   })
-// })
+router.get("/:userID/details/:customerID", (req, res) => {
+  Customer.findOne( {
+    where: {
+      userID: req.params.userID,
+      id : req.params.customerID
+    },
+    include: [
+      { model: Payment },
+      { model: Note },
+      { model: CustomerBalance }
+    ]
+  }).then(customer => {
+    if(customer) {
+      res.send({
+        status: "success",
+        customer: customer
+      })
+    } else {
+      res.json({ 
+        status: "error", 
+        message: "customer not found"})
+    }
+  })
+})
 
 router.post("/add", (req, res) => {
   const { userID, costumerInfo, costumerName, phoneNumber, taxNumber, taxAdress } = req.body;
