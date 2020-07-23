54420da286990871336c5602687d51a58582885b
suhacanuluer <sculuer@gmail.com>
2020-07-23 19:15:50 +0300

    payments route updated

--- routes/payments.js
+++ routes/payments.js
@@ -1,55 +1,117 @@
 const express = require("express");
 const router = express.Router();
-const { Payment, CustomerBalance, sequelize } = require("../Database/Database");
+const { Payment, CustomerBalance, UserBalance } = require("../Database/Database");
 
-//TAMAMEN GÖZDEN GEÇİRİLECEK ÖDEMELER BALANCE MODELLERİNE AKTARILACAK
+
 router.post("/add", (req, res) => {
-    const { customerID, cost, infoKDV, inOrOut, date } = req.body;
-    
-    Payment.create(req.body).then( payments => {
+    const { userID, customerID, cost, infoKDV, inOrOut, date } = req.body;
 
-//aşağıdaki findone ı createnin içine alıp tüm çıktıları aynı res içinde döndürmen lazım 
-        res.json(payments.toJSON());
+// ÖDEME İŞLEMİ OLUŞTURULDU
+    Payment.create(req.body).then(payment => {
+        res.json({
+            status: "success",
+            data: payment
+        });
     }, (e) => {
-        res.status(500).send({ status: "error"})
-    })
+        res.status(500).json({
+            status: "error"
+        });
+    });
 
-    let balance = { customerID: customerID};    
+// ÖDEMELER ÜZERİNDE HESAPLAMALAR YAPILIYOR
+    let payment = { customerID: customerID };
     if(inOrOut == true) {
         if(infoKDV == true) {
-            balance.inMoneyVAT = cost;
-            balance.amountVAT = ( cost / 100 * 18 );
-            balance.inMoney = 0;
-            balance.outMoney = 0;
+            payment.inMoneyVAT = cost;
+            payment.amountVAT = ( cost / 100 * 18 );
+            payment.inMoney = 0;
+            payment.outMoney = 0;
+        } else {
+            payment.inMoneyVAT = 0;
+            payment.amountVAT = 0;
+            payment.inMoney = cost;
+            payment.outMoney = 0;
         }
-    }
+    } else {
+        payment.inMoneyVAT = 0;
+        payment.amountVAT = 0;
+        payment.inMoney = 0;
+        payment.outMoney = cost;
+    }    
 
     CustomerBalance.findOne({
-        where: {
+        where: { 
             customerID: customerID
         }
-    }).then(data => {
-        if(data == null){
-            balance.totalMoney = balance.inMoneyVAT + balance.inMoney + balance.outMoney;
-            CustomerBalance.create(balance).then(balances => {
-                console.log("balance", balance)
-                console.log("balances", balances)
-                res.json({  // <<< mesela bu resi yukarı alman lazm 
-                    status: "success",
-                    data: balances
-                });
-            }, (e) => {
-                // res.status(500).send({ status: "error"})
-            })
+    }).then(oldCustomerBalance => {
+        payment.totalMoney = ( payment.inMoneyVAT + payment.inMoney - payment.outMoney );
+        if(oldCustomerBalance == null) {
+            CustomerBalance.create(payment).then(newCustomerBalance => {
+                updatedUserBalance(newCustomerBalance);
+            });
         } else {
-            console.log("data var")
-        }    
-    })
+            let currentCustomerBalance = oldCustomerBalance.dataValues;
+            currentCustomerBalance.inMoneyVAT += payment.inMoneyVAT;
+            currentCustomerBalance.amountVAT += payment.amountVAT;
+            currentCustomerBalance.inMoney += payment.inMoney;
+            currentCustomerBalance.outMoney += payment.outMoney;
+            currentCustomerBalance.totalMoney += payment.totalMoney;
+            currentCustomerBalance.customerID = customerID
+
+            CustomerBalance.update(
+                currentCustomerBalance,
+                { where: {
+                    customerID: customerID
+                }} 
+                ).then(finalCustomerBalance => {
+                    updatedUserBalance(finalCustomerBalance);
+                });
+            }
+        });
+
+        function updatedUserBalance(customerBalance) {
+            let userBalance = customerBalance.dataValues;
+            console.log("userBalance", userBalance);
+            console.log("CUSTOMERBalance", customerBalance);
+            UserBalance.findOne({
+                where: {
+                    userID: userID
+                }
+            }).then(oldUserBalance => {
+                if(oldUserBalance == null) {
+                    payment.userID = userID;
+                    UserBalance.create(payment).then(newUserBalance => {
+                        console.log("newUserBalance", newUserBalance);
+                    })
+                } else {
+                    let currentUserBalance = oldUserBalance.dataValues;
+                    currentUserBalance.inMoneyVAT += payment.inMoneyVAT;
+                    currentUserBalance.amountVAT += payment.amountVAT;
+                    currentUserBalance.inMoney += payment.inMoney;
+                    currentUserBalance.outMoney += payment.outMoney;
+                    currentUserBalance.totalMoney += payment.totalMoney;
+                    currentUserBalance.userID = userID
     
+                    UserBalance.update(
+                        currentUserBalance,
+                        { where: {
+                            userID: userID
+                        }}
+                    ).then(finalUserBalance => {
+                        console.log("finalUserBalance", finalUserBalance)
+                    });
+                }
+            });
+        };
+
+
     
 
     
-})
+
+
+});
+
 
 module.exports = router;
 
